ef1ad52b892f28acb997eff2c505adea6fb87d20
Ryane Chia <ryanechia@outlook.com>
2018-11-13 04:13:33 +0800

    return results

--- addon/services/humanize.js
+++ addon/services/humanize.js
@@ -5,69 +5,69 @@ export default Service.extend({
 
   //numbers
   formatNumber(number, precision = 0, thousand = ',', decimal = '.') {
-    Humanize.formatNumber(number, precision, thousand, decimal);
+    return Humanize.formatNumber(number, precision, thousand, decimal);
   },
   intComma(number, decimals = 0) {
-    Humanize.intComma(number, decimals);
+    return Humanize.intComma(number, decimals);
   },
   compactInteger(input, decimals = 0) {
-    Humanize.compactInteger(input, decimals);
+    return Humanize.compactInteger(input, decimals);
   },
   boundedNumber(num, bound = 100, ending = '+') {
-    Humanize.boundedNumber(num, bound, ending);
+    return Humanize.boundedNumber(num, bound, ending);
   },
   ordinal(value) {
-    Humanize.ordinal(value);
+    return Humanize.ordinal(value);
   },
   times(value, overrides = {}) {
-    Humanize.times(value, overrides);
+    return Humanize.times(value, overrides);
   },
   pace(value, intervalMs, unit = 'time') {
-    Humanize.pace(value, intervalMs, unit);
+    return Humanize.pace(value, intervalMs, unit);
   },
   fileSize(filesize, precision = 2) {
-    Humanize.fileSize(filesize, precision);
+    return Humanize.fileSize(filesize, precision);
   },
 
   //Strings
   pluralize(number, singular, plural) {
-    Humanize.pluralize(number, singular, plural);
+    return Humanize.pluralize(number, singular, plural);
   },
   truncate(str, length = 100, ending = '...') {
-    Humanize.truncate(str, length, ending);
+    return Humanize.truncate(str, length, ending);
   },
   truncateWords(string, length) {
-    Humanize.truncateWords(string, length);
+    return Humanize.truncateWords(string, length);
   },
   nl2br(string, replacement = '<br/>') {
-    Humanize.nl2br(string, replacement);
+    return Humanize.nl2br(string, replacement);
   },
   br2nl(string, replacement = '\r\n') {
-    Humanize.br2nl(string, replacement);
+    return Humanize.br2nl(string, replacement);
   },
   capitalize(string, downCaseTail = false) {
-    Humanize.capitalize(string, downCaseTail);
+    return Humanize.capitalize(string, downCaseTail);
   },
   capitalizeAll(string) {
-    Humanize.capitalizeAll(string);
+    return Humanize.capitalizeAll(string);
   },
   titleCase(string) {
-    Humanize.titleCase(string);
+    return Humanize.titleCase(string);
   },
 
   //arrays
   oxford(items, limit, limitStr) {
-    Humanize.oxford(items, limit, limitStr);
+    return Humanize.oxford(items, limit, limitStr);
   },
-  frequency(list, verb){
-    Humanize.frequency(list, verb);
+  frequency(list, verb) {
+    return Humanize.frequency(list, verb);
   },
 
   //utility
-  toFixed(value, precision){
-    Humanize.toFixed(value, precision);
+  toFixed(value, precision) {
+    return Humanize.toFixed(value, precision);
   },
-  normalizePrecision(value, base){
-    Humanize.normalizePrecision(value, base);
+  normalizePrecision(value, base) {
+    return Humanize.normalizePrecision(value, base);
   }
 });
